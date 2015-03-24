09f052b08f0f6c5f011b4041a565f9be2c3304ce
Kristin <dylanfan01@gmail.com>
2015-03-23 21:26:28 -0700

    Created a detail page about each victim

--- app.py
+++ app.py
@@ -10,13 +10,19 @@ def get_csv():
     csv_list = list(csv_obj)
     return csv_list
 
-
 @app.route("/")
 def index():
 	template = 'index.html'
 	object_list = get_csv()
 	return render_template(template, object_list=object_list)
-	
 
+@app.route('/<row_id>/')
+def detail(row_id):
+    template = 'detail.html'
+    object_list = get_csv()
+    for row in object_list:
+        if row['id'] == row_id:
+            return render_template(template, object=row)
+	
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
